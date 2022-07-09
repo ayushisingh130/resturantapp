6b8997485705295bc590dd60472a29dd5279ebac
Ayushi <ayushisingh0713@gmail.com>
2022-07-09 14:15:36 +0530

    first commit

---
+++ src/utils/firebaseFunctions.js
@@ -0,0 +1,26 @@
+import {
+  collection,
+  doc,
+  getDocs,
+  orderBy,
+  query,
+  setDoc,
+} from "firebase/firestore";
+import { firestore } from "../firebase.config";
+
+// Saving new Item
+const saveItem = async (data) => {
+  await setDoc(doc(firestore, "foodItems", `${Date.now()}`), data, {
+    merge: true,
+  });
+};
+
+const getAllFoodItems = async () => {
+  const items = await getDocs(
+    query(collection(firestore, "foodItems"), orderBy("id", "desc"))
+  );
+
+  return items.docs.map((doc) => doc.data());
+};
+
+export { saveItem, getAllFoodItems };
